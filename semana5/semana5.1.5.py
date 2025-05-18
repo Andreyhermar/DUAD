8297e76675eebca5782bb016ad4b763a9848d02b
jhernandezma <jhernandezma@ucenfotec.ac.cr>
2025-05-18 17:43:06 -0600

    Revert "Past exercises"
    This reverts commit c447eae990dd2a56b1e2c99e7787fe78046e3d2f.

--- semana5/semana5.1.5.py
+++
@@ -1,8 +0,0 @@
-my_list = []
-print("ingrese 10 valores y luego le mostraremos el valor mas alto")
-for i in range(10):
-    numero = int(input(f"ingrese el numero {i+1}: "))
-    my_list.append(numero)
-print("Los numeros ingresados son:", my_list)
-highest_number = max(my_list)
-print("El valor mas alto es: ", highest_number)
