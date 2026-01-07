c783bbaabf65b7d2c0aa78546f68ac7a1e11c964
jhernandezma <jhernandezma@ucenfotec.ac.cr>
2026-01-07 17:11:16 -0600

    semana11 OOP

--- semana6/semana6.1.py
+++
@@ -1,8 +0,0 @@
-def first_function():
-	print("Hello World!")
-	second_function()
-
-def second_function():
-	print("Happy Monday")
-
-first_function()
--- semana6/semana6.4.py
+++
@@ -1,6 +0,0 @@
-def turn_it_over():
-    word = "Hola Mundo"
-    #for i in range(len(word) -1, -1, -1):
-    print(word[::-1])
-
-turn_it_over()
